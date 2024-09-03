2720f5f54d06ec0abed6e77c47ec8a1bebdc9e30
MarcosC <mcouttulenc@fi.uba.ar>
2024-09-03 18:47:40 -0300

    cambios en el server para que se cierre gracefully

--- server/common/server.py
+++ server/common/server.py
@@ -1,5 +1,8 @@
 import socket
 import logging
+import signal
+import threading
+import errno
 
 
 class Server:
@@ -8,6 +11,7 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._shutdown_event = threading.Event()
 
     def run(self):
         """
@@ -18,11 +22,43 @@ class Server:
         finishes, servers starts to accept new connections again
         """
 
+        # Apenas se lanza el server, se setea el manejo de la signal SIGTERM
+        # en el metodo _graceful_shutdown
+        signal.signal(signal.SIGTERM, self._graceful_shutdown)
+
+
+
+        try:
+            # una vez que llegue la signal SIGTERM, el ciclo se corta           
+            while not self._shutdown_event.is_set():
+                client_sock = self.__accept_new_connection()
+                # client_sock puede ser un None si llego la SIGTERM mientras
+                # el socket estaba trabado en accept(), que es lo mas probable
+                # que pase siempre.
+                if client_sock:
+                    self.__handle_client_connection(client_sock)
+        except OSError as e:
+            # por si el socket levanta esta excepcion al recibir la signal SIGTERM, creo que no es necesario
+            if e.errno == errno.EINTR:
+                logging.info('SOCKET CERRADO - RUN')
+            else:
+                logging.info('Unknown exception.')
+                raise
+
+
+
         # TODO: Modify this program to handle signal to graceful shutdown
         # the server
-        while True:
-            client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(client_sock)
+        #while True:
+        #    client_sock = self.__accept_new_connection()
+        #    self.__handle_client_connection(client_sock)
+    
+
+    def _graceful_shutdown(self, signum, frame):
+        logging.info(f'RECIBI SIGTERM, SE EJECUTA GRACEFUL_SHUTDOWN')
+        self._shutdown_event.set()
+        self._server_socket.close()  # Cerrar el socket para desbloquear accept()
+
 
     def __handle_client_connection(self, client_sock):
         """
@@ -42,6 +78,9 @@ class Server:
             logging.error("action: receive_message | result: fail | error: {e}")
         finally:
             client_sock.close()
+    
+
+    
 
     def __accept_new_connection(self):
         """
@@ -51,8 +90,21 @@ class Server:
         Then connection created is printed and returned
         """
 
-        # Connection arrived
+
         logging.info('action: accept_connections | result: in_progress')
-        c, addr = self._server_socket.accept()
-        logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
-        return c
+        try:
+            c, addr = self._server_socket.accept()
+            logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
+            return c
+        except OSError as e:
+            if e.errno == errno.EBADF:  # Bad file descriptor, server socket closed
+                logging.info('SOCKET CERRADO - ACCEPT_NEW_CONNECTIONS')
+                return None
+            else:
+                raise
+
+        # Connection arrived
+        #logging.info('action: accept_connections | result: in_progress')
+        #c, addr = self._server_socket.accept()
+        #logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
+        #return c
