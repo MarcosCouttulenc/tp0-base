5fa897381ee2a95a78ae4b0e7cc0af7e8cfae038
MarcosC <mcouttulenc@fi.uba.ar>
2024-09-05 05:03:43 -0300

    cambios en la generacion del docker compose dev y en una comparacion del sh

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -9,7 +9,7 @@ MENSAJE_DE_PRUEBA="Hello World!"
 
 MENSAJE_RECIBIDO=$(docker run --rm --network tp0_testing_net busybox sh -c "echo \"$MENSAJE_DE_PRUEBA\" | nc \"$NOMBRE_CONTAINER_SERVER\" \"$PUERTO\"")
 
-if [ "$MENSAJE_RECIBIDO" == "$MENSAJE_DE_PRUEBA" ]; then
+if [ "$MENSAJE_RECIBIDO" = "$MENSAJE_DE_PRUEBA" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
